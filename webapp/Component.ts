157ead64dfd76bd769a25536cddb16bead532dda
Jb <jvwbrown0@gmail.com>
2026-07-07 09:18:55 +0200

    Implement Central Login Popover functionality in Component and remove unused code from Main controller

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -1,26 +1,104 @@
 import BaseComponent from "sap/ui/core/UIComponent";
+import Fragment from "sap/ui/core/Fragment";
+import Popover from "sap/m/Popover";
+import Event from "sap/ui/base/Event";
+import Control from "sap/ui/core/Control";
 import { createDeviceModel } from "./model/models";
+
+type ShellRenderer = {
+    addHeaderEndItem: (
+        properties: Record<string, unknown>,
+        isVisible: boolean,
+        isCurrentState: boolean
+    ) => void;
+};
+
+const POPOVER_FRAGMENT_NAME = "brown.centralewmlogin.fragments.CentralLoginPopover";
 
 /**
  * @namespace brown.centralewmlogin
  */
 export default class Component extends BaseComponent {
 
-	public static metadata = {
-		manifest: "json",
+    private loginPopover: Popover | undefined;
+    private headerButtonAdded = false;
+
+    public static metadata = {
+        manifest: "json",
         interfaces: [
             "sap.ui.core.IAsyncContentCreation"
         ]
-	};
+    };
 
-	public init() : void {
-		// call the base component's init function
-		super.init();
-
-        // set the device model
+    public init(): void {
+        super.init();
         this.setModel(createDeviceModel(), "device");
-
-        // enable routing
         this.getRouter().initialize();
-	}
+        this.registerHeaderLoginButton();
+    }
+
+    private registerHeaderLoginButton(): void {
+        if (this.headerButtonAdded) {
+            return;
+        }
+
+        const ushellContainer = (window as {
+            sap?: {
+                ushell?: {
+                    Container?: {
+                        getRenderer?: (name: string) => ShellRenderer | undefined;
+                        attachRendererCreatedEvent?: (callback: () => void) => void;
+                    };
+                };
+            };
+        }).sap?.ushell?.Container;
+
+        const renderer = ushellContainer?.getRenderer?.("fiori2");
+
+        if (!renderer) {
+            ushellContainer?.attachRendererCreatedEvent?.(() => {
+                this.registerHeaderLoginButton();
+            });
+
+            return;
+        }
+
+        renderer.addHeaderEndItem(
+            {
+                id: this.createId("openLoginButton"),
+                icon: "sap-icon://BusinessSuiteInAppSymbols/icon-refinery",
+                tooltip: "Open Login",
+                press: [this.onOpenLoginPress, this]
+            },
+            true,
+            false
+        );
+
+        this.headerButtonAdded = true;
+    }
+
+    public async onOpenLoginPress(oEvent: Event): Promise<void> {
+        const loginPopover = await this.getOrCreateLoginPopover();
+
+        if (!loginPopover) {
+            return;
+        }
+
+        loginPopover.openBy(oEvent.getSource() as Control);
+    }
+
+    private async getOrCreateLoginPopover(): Promise<Popover | undefined> {
+        if (this.loginPopover) {
+            return this.loginPopover;
+        }
+
+        this.loginPopover = await Fragment.load({
+            id: this.createId("centralLoginPopover"),
+            name: POPOVER_FRAGMENT_NAME
+        }) as Popover;
+
+        this.getRootControl()?.addDependent(this.loginPopover);
+
+        return this.loginPopover;
+    }
 }
--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,50 +1,10 @@
 import Controller from "sap/ui/core/mvc/Controller";
-import XMLView from "sap/ui/core/mvc/XMLView";
-import Popover from "sap/m/Popover";
-import Event from "sap/ui/base/Event";
-import Button from "sap/m/Button";
 
 
 /**
- * @namespace brown.centralewmlogin.fragments.CentralLoginPopover
+ * @namespace brown.centralewmlogin.controller
  */
 export default class Main extends Controller {
 
-    private _oPopover: Popover | undefined;
-    private _oPopupView: XMLView | undefined;
-
     public onInit(): void { }
-
-    public async onOpenLoginPress(oEvent: Event): Promise<void> {
-        if (!this._oPopover) {
-            const oView = this.getView();
-
-            if (!oView) {
-                return;
-            }
-
-            this._oPopupView = await XMLView.create({
-                id: oView.getId(),
-                viewName: "brown.centralewmlogin.view.Popup",
-                controller: this
-            });
-
-            this._oPopover = this._oPopupView.byId("centralLoginPopover") as Popover | undefined;
-
-            if (!this._oPopover) {
-                return;
-            }
-
-            oView.addDependent(this._oPopupView);
-        }
-
-        const oButton = oEvent.getSource() as Button;
-        const oPopover = this._oPopover;
-
-        if (!oPopover) {
-            return;
-        }
-
-        oPopover.openBy(oButton);
-    }
 }
