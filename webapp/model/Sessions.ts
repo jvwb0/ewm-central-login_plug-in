0794fba92c2d58cbddf8ffd02f85abde660625fe
Jb <jvwbrown0@gmail.com>
2026-07-09 14:28:42 +0200

    Implement loadSession method to retrieve and validate session data from localStorage

--- webapp/model/Sessions.ts
+++ webapp/model/Sessions.ts
@@ -1,13 +1,31 @@
 import type { PluginState } from "./Types";
 
 export default class Sessions {
+
     saveSession(state: PluginState): void {
         const dataToStore = { state: state, savedAt: Date.now() };       // Bundle the state with a timestamp into one object
         const asString = JSON.stringify(dataToStore);                    // Convert the object to a string (localStorage only holds strings)
         localStorage.setItem("brown.centralewmlogin.session", asString); // Write it to browser storage under a key
     }
 
-    loadSession(state: PluginState | null): void { }
+    loadSession(): PluginState | null {
+        const raw = localStorage.getItem("brown.centralewmlogin.session");
+        if (!raw) {
+            return null; // if null, undefined, "", 0, false, NaN 
+        }
+        try {
+            const parsed = JSON.parse(raw) as { state: PluginState; savedAt: number }; // Parse the string back into an object
+            if (Date.now() - parsed.savedAt > 24 * 60 * 60 * 1000) { // If the saved session is older than 24 hours, discard it
+                this.clearSession();
+                return null;
+            }
+
+            return parsed.state;
+        } catch (e) {
+            this.clearSession(); // if JSON.parse fails, clear corrupt data & return null
+            return null;
+        }
+    }
 
     clearSession(): void {
         localStorage.removeItem("brown.centralewmlogin.session");
